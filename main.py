f5a292c355ea0bd33352b3ee9310e77628104e01
Sina Sun <sinakhodaveisi@gmail.com>
2023-05-27 11:02:59 -0400

    Args added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import argparse
 
 from crawler.parser import parse_link
 from crawler.storage import *
 from crawler.utils import *
 
 def crawl_link(domain_path, link, domain):
+
 	append_to_file(domain_path + "/scraped.txt", link + "\n")
 	
 	data = parse_link(link, domain)
@@ -15,19 +17,27 @@ def crawl_link(domain_path, link, domain):
 			crawl_link(domain_path, url, domain)
 
 
-def crawl_website(url):
-	normalized_url = normalize_link(url)
+def crawl_website(url, output_folder):
+
 	domain = get_domain_from_link(url)	
-	domain_folder = "data/" + domain
+	domain_folder = output_folder + domain
 	create_folder(domain_folder)
 	
 	crawl_link(domain_folder, url, domain)
 	
 if __name__ == '__main__':
+		
+	parser = argparse.ArgumentParser(prog="WebCrawler", description="Python Web Crawler", epilog="python3 main.py -w https://example.com -o data/")
+
+	parser.add_argument('-w', '--website', help="The website url to crawl")
+	parser.add_argument('-o', '--output', default="data/", help="Output folder")
+
+	args = parser.parse_args()
+
+	output_folder = args.output
+	website_url = args.website
 	
-	create_folder("data")
-	website_url = 'https://speedyjanitorial.ca/'
-
-	crawl_website(website_url)
-
+	
+	create_folder(output_folder)
+	crawl_website(website_url, output_folder)
 
